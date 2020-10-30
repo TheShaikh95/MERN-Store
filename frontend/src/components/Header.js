a58d8b248375a8ec48435c3ff531146998057fd1
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-10-29 22:05:18 -0400

    added fontawesome and some icons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,14 +5,16 @@ const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+                <Container>
   <Navbar.Brand href="/">Afnan's MERN store!</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/cart">Cart</Nav.Link>
-      <Nav.Link href="/login">Sign In</Nav.Link>
+    <Nav className="ml-auto">
+      <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+      <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
     </Nav>
   </Navbar.Collapse>
+  </Container>
 </Navbar>
         </header>
     )
