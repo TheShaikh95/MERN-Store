47ddb448b028a73e94e5c32f6ad12c978c4c944e
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-11-01 22:35:05 -0500

    pirce and description

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -27,6 +27,8 @@ const ProductScreen = ({match}) => {
                     text={`${product.numReviews} reviews`} 
                     />
                 </ListGroup.Item>
+                <ListGroup.Item> Price: ${product.price} </ListGroup.Item>
+                <ListGroup.Item>Description: {product.description}</ListGroup.Item>
             </ListGroup>
             </Col>
         </Row>
