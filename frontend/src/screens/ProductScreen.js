4543a4cd782b9c787d04530a5f088b0dcba74e77
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-11-01 22:48:39 -0500

    added structure to add to cart feature

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -31,6 +31,34 @@ const ProductScreen = ({match}) => {
                 <ListGroup.Item>Description: {product.description}</ListGroup.Item>
             </ListGroup>
             </Col>
+            <Col md={3}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Price:</Col>
+                                <Col>
+                            <strong>${product.price}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Status:</Col>
+                                <Col>
+                             {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button className='btn-block' type='button'>
+                                Add to cart
+                            </Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
         </Row>
     </>
     ) 
