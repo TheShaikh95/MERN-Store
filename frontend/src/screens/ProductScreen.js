46860bed2fe583060e6ebc4fa2445678a418036f
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-11-01 22:28:10 -0500

    images edits

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form, ListGroupItem } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import products from '../products'
 
@@ -12,6 +12,18 @@ const ProductScreen = ({match}) => {
     <Link className='btn btn-light my-3' to='/'>
         Go Back
         </Link>
+        <Row>
+            <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid /> 
+            </Col>
+            <Col md={3}>
+            <ListGroup variant='flush'>
+                <ListGroup.Item>
+    <h2>{product.name}</h2>
+                </ListGroup.Item>
+            </ListGroup>
+            </Col>
+        </Row>
     </>
     ) 
 }
