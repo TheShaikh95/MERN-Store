7bcd4b674cae22f9ee48dc768385506a1a787112
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-11-01 22:20:37 -0500

    go back button added

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,8 +7,13 @@ import products from '../products'
 const ProductScreen = ({match}) => {
     const product = products.find(p => p._id === match.params.id)
 
-    return <div>{product.name}</div>
-    
+    return (
+    <>
+    <Link className='btn btn-light my-3' to='/'>
+        Go Back
+        </Link>
+    </>
+    ) 
 }
 
 export default ProductScreen
