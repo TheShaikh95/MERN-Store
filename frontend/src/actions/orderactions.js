77007e5269414f1a152b6a1d6b0ecd7670937c25
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-11-04 20:35:30 -0500

    fixing more bugs

--- frontend/src/actions/orderactions.js
+++ frontend/src/actions/orderactions.js
@@ -38,3 +38,45 @@ export const createOrder = (order) => async (dispatch, getState) => {
           Authorization: `Bearer ${userInfo.token}`,
         },
       }
+
+      const { data } = await axios.post(`/api/orders`, order, config)
+
+    dispatch({
+      type: ORDER_CREATE_SUCCESS,
+      payload: data,
+    })
+    dispatch({
+      type: CART_CLEAR_ITEMS,
+      payload: data,
+    })
+    localStorage.removeItem('cartItems')
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload: message,
+    })
+  }
+}
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_DETAILS_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
