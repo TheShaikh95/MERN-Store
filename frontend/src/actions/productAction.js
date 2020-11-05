ec3ffcd6827f0017d7ac794a716cab88bf80725a
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-11-04 20:38:34 -0500

    commiting entire file together

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -105,3 +105,143 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
     })
   }
 }
+
+export const createProduct = () => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: PRODUCT_CREATE_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.post(`/api/products`, {}, config)
+  
+      dispatch({
+        type: PRODUCT_CREATE_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: PRODUCT_CREATE_FAIL,
+        payload: message,
+      })
+    }
+  }
+  
+  export const updateProduct = (product) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: PRODUCT_UPDATE_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.put(
+        `/api/products/${product._id}`,
+        product,
+        config
+      )
+  
+      dispatch({
+        type: PRODUCT_UPDATE_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: PRODUCT_UPDATE_FAIL,
+        payload: message,
+      })
+    }
+  }
+  
+  export const createProductReview = (productId, review) => async (
+    dispatch,
+    getState
+  ) => {
+    try {
+      dispatch({
+        type: PRODUCT_CREATE_REVIEW_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      await axios.post(`/api/products/${productId}/reviews`, review, config)
+  
+      dispatch({
+        type: PRODUCT_CREATE_REVIEW_SUCCESS,
+      })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: PRODUCT_CREATE_REVIEW_FAIL,
+        payload: message,
+      })
+    }
+  }
+  
+  export const listTopProducts = () => async (dispatch) => {
+    try {
+      dispatch({ type: PRODUCT_TOP_REQUEST })
+  
+      const { data } = await axios.get(`/api/products/top`)
+  
+      dispatch({
+        type: PRODUCT_TOP_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_TOP_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      })
+    }
+  }
