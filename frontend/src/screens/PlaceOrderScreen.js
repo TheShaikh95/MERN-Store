4ebb955c66d9303694fad1ff5e86286dd365f11c
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-11-04 19:22:07 -0500

    fixing bugs

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -36,3 +36,41 @@ const PlaceOrderScreen = ({ history }) => {
       history.push(`/order/${order._id}`)
       dispatch({ type: ORDER_CREATE_RESET })
     }
+
+}, [history, success])
+
+const placeOrderHandler = () => {
+  dispatch(
+    createOrder({
+      orderItems: cart.cartItems,
+      shippingAddress: cart.shippingAddress,
+      paymentMethod: cart.paymentMethod,
+      itemsPrice: cart.itemsPrice,
+      shippingPrice: cart.shippingPrice,
+      taxPrice: cart.taxPrice,
+      totalPrice: cart.totalPrice,
+    })
+  )
+}
+
+return (
+  <>
+    <CheckoutSteps step1 step2 step3 step4 />
+    <Row>
+      <Col md={8}>
+        <ListGroup variant='flush'>
+          <ListGroup.Item>
+            <h2>Shipping</h2>
+            <p>
+              <strong>Address:</strong>
+              {cart.shippingAddress.address}, {cart.shippingAddress.city}{' '}
+              {cart.shippingAddress.postalCode},{' '}
+              {cart.shippingAddress.country}
+            </p>
+          </ListGroup.Item>
+
+          <ListGroup.Item>
+            <h2>Payment Method</h2>
+            <strong>Method: </strong>
+            {cart.paymentMethod}
+          </ListGroup.Item>
