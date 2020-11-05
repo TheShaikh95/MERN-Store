bee17245dfa4c1d1602f6f95d25bb1d4019f43d3
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-11-04 19:17:53 -0500

    adding the payment screen

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { Form, Button, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
+import { savePaymentMethod } from '../actions/cartActions'
+
+const PaymentScreen = ({ history }) => {
+  const cart = useSelector((state) => state.cart)
+  const { shippingAddress } = cart
+
+  if (!shippingAddress) {
+    history.push('/shipping')
+  }
+
+  const [paymentMethod, setPaymentMethod] = useState('PayPal')
+
+  const dispatch = useDispatch()
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(savePaymentMethod(paymentMethod))
+    history.push('/placeorder')
+  }
